389a95e843984d28c5f0cd2b4b306f153c1a0e94
mikevanviegen <mike.van.viegen@rabobank.nl>
2021-12-01 20:16:22 +0100

    Updated test for input

--- src/test/kotlin/day/one/OneTest.kt
+++ src/test/kotlin/day/one/OneTest.kt
@@ -5,8 +5,6 @@ import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 internal class OneTest {
-    private val dayOneInput = this::class.java.getResource("/dayoneinput.txt").readText()
-
     @Test
     internal fun `should return 7 given a list with 7 increments`() {
         val input = listOf(199,200,208,210,200,207,240,269,260,263)
@@ -17,6 +15,19 @@ internal class OneTest {
 
     @Test
     internal fun `should return the amount of increments of a given list`() {
-        // Blabla valideer iets uit dayOneInput var
+        val input = generateListOfInput()
+        println(input.first())
+        val result = One().measureIncrements(input)
+
+        assertThat(result).isEqualTo(7)
+    }
+
+    private fun generateListOfInput(): MutableList<Int> {
+        val file = this::class.java.getResource("/dayoneinput.txt").openStream().bufferedReader()
+        var inputList = mutableListOf<Int>()
+        for (line in file.readLines()) {
+            inputList.add(line.toInt())
+        }
+        return inputList
     }
 }
